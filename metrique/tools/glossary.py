8e58fb4a734003875b76989b9f88b1a665994175
Chris Ward <cward@redhat.com>
2013-05-31 10:08:04 +0200

    committing missing files; glossary and client/cubes/__init__

---
+++ metrique/tools/glossary.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+# Author: "Chris Ward" <cward@redhat.com>
+
+
+def join_and(*args):
+    return ' and '.join(args)
+
+
+def join_or(*args):
+    return ' or '.join(args)
+
+
+def o_in(field_token):
+    return '%s in [%s]' % field_token
+
+
+def o_has(field_token):
+    if isinstance(field_token, str):
+        return '%s == "%s"' % field_token
+    else:
+        return '%s == %s' % field_token
+
+
+def o_matches(field_token):
+    return '%s == regex("%s")' % field_token
