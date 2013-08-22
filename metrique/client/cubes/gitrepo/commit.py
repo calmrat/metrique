a350ee424b6d4f63cd755b48465d89ae1f8e58a3
Chris Ward <cward@redhat.com>
2013-08-22 07:58:30 +0200

    refactor gitrepo.commit; now ussing gittle

--- metrique/client/cubes/gitrepo/commit.py
+++ metrique/client/cubes/gitrepo/commit.py
@@ -8,6 +8,7 @@ import os
 import subprocess
 
 from metrique.client.cubes.basegitobject import BaseGitObject
+from metrique.tools import oid as new_oid
 
 related_re = re.compile('Related: (.+)$', re.I)
 resolves_re = re.compile('Resolves: (.+)$', re.I)
@@ -21,67 +22,42 @@ class Commit(BaseGitObject):
     """
     Object used for communication with Git Commit interface
     """
-
     name = 'git_commit'
 
-    defaults = {
-        'index': True,
-    }
-
-    def extract(self, uri, fetch=True, **kwargs):
+    def extract(self, uri, fetch=True, shas=None, force=False,
+            **kwargs):
+        if shas is None:
+            shas = []
         self.logger.debug("Extracting GIT repo: %s" % uri)
-        repo = self.get_repo(uri, fetch)
-        last_ts = self.find('uri == "%s"' % uri, fields='_commit_ts',
-                            sort=[('_commit_ts', -1)], one=True, raw=True)
-        if last_ts:
-            last_ts = last_ts['_commit_ts'] + 0.1
-        self.logger.debug("Last Commit Date: %s" % last_ts)
+        self.repo = self.get_repo(uri, fetch)
+        self.commits = tuple(self.repo.commits())
+        if not (force or shas):
+            last_ts = self.find(
+                    'uri == "%s"' % uri, fields='_commit_ts',
+                    sort=[('_commit_ts', -1)], one=True, raw=True)
+            if last_ts:
+                last_ts = last_ts['_commit_ts'] + 0.1
+            self.logger.debug("Last Commit Date: %s" % last_ts)
         self.stats = {}
         batch = []
-        for walk_entry in repo.get_walker(since=last_ts):
-            batch.append(self.get_commit(repo, walk_entry, uri))
+        objs = self.repo.commit_info()
+        for obj in objs:
+            obj['_oid'] = new_oid()
+            obj['uri'] = uri
+            sha = obj['sha']
+            index = self.commits.index(sha)
+            obj['stats'] = self.get_stats(sha)
+            msg = obj['message']
+
+            obj['acked_by'] = acked_by_re.findall(msg)
+            obj['signed_off_by'] = signed_off_by_re.findall(msg)
+            obj['resolves'] = resolves_re.findall(msg)
+            obj['related'] = related_re.findall(msg)
+
+            obj['diffs'] = self.repo.diff(sha)
+
+            batch.append(obj)
         return self.save_objects(batch)
-
-    def get_commit(self, repo, walk_entry, uri):
-        c = walk_entry.commit
-        if c.type != 1:
-            raise TypeError(
-                "Expected 'commit' type objects. Got (%s)" % c.type)
-
-        msg = c.message
-
-        acked_by = acked_by_re.findall(msg)
-        signed_off_by = signed_off_by_re.findall(msg)
-        resolves = resolves_re.findall(msg)
-        related = related_re.findall(msg)
-
-        obj = {
-            'uri': uri,
-            'id': c.id,
-            'parents': c.parents,
-            'author': c.author,
-            'author_dt': datetime.fromtimestamp(c.author_time +
-                                                c.author_timezone),
-            'committer': c.committer,
-            'commit_dt': datetime.fromtimestamp(c.commit_time +
-                                                c.commit_timezone),
-            '_commit_ts': c.commit_time,
-            # FIXME: Can this be sped up?
-            # These commented out are all very slow
-            #'count': c.count(),
-            #'name_rev': c.name_rev,
-            #'size': c.size,
-            'files': self.get_stats(c)['files'],
-            'stats': self.get_stats(c)['stats'],
-            'summary': c.message.split('\n', 1)[0],
-            'message': c.message,
-            'acked_by': acked_by,
-            'signed_off_by': signed_off_by,
-            'resolves': resolves,
-            'related': related,
-        }
-
-        return obj
 
     def save_stats(self, sha, stats):
         files = []
@@ -117,12 +93,12 @@ class Commit(BaseGitObject):
                     stats.append(line.strip())
             self.save_stats(last_hash, stats)
 
-        if commit.id not in self.stats:
+        if commit not in self.stats:
             os.chdir(self.repo_path)
-            cmd = 'git diff --numstat %s' % commit.id
+            cmd = 'git diff --numstat %s' % commit
             proc = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE)
-            self.save_stats(commit.id, proc.stdout.readlines())
-        return self.stats[commit.id]
+            self.save_stats(commit, proc.stdout.readlines())
+        return self.stats[commit]
 
 if __name__ == '__main__':
     from metrique.client.argparsers import cube_cli
