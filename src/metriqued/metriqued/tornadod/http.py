3d617aa4533569dc67755eafcfd053baf119915f
Juraj Niznan <jurajniznan@gmail.com>
2013-09-12 17:43:42 +0200

    server stopping

--- src/metriqued/metriqued/tornadod/http.py
+++ src/metriqued/metriqued/tornadod/http.py
@@ -6,8 +6,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 import os
+import signal
+import time
 from tornado.ioloop import IOLoop
 from tornado.web import Application
+from tornado.httpserver import HTTPServer as Server
 
 from metriqued.metriqueserver import MetriqueServer
 
@@ -137,33 +140,58 @@ class HTTPServer(MetriqueServer):
                 certfile=self.metrique_config.ssl_certificate,
                 keyfile=self.metrique_config.ssl_certificate_key)
             try:
-                self._web_app.listen(port=port, address=address,
-                                     ssl_options=ssl_options)
+                self.server = Server(self._web_app, ssl_options=ssl_options)
             except ValueError:
                 raise ValueError(
                     "SSL Cert missing, perhaps? (%s)." % ssl_options)
         else:
-            self._web_app.listen(port=port, address=address)
+            self.server = Server(self._web_app)
 
+        self.server.listen(port=port, address=address)
         logger.debug("Tornado: listening on %s:%s" % (port, address))
 
     def start(self):
         ''' Start a new tornado web app '''
         pid = os.fork()
         if pid == 0:
-            logger.debug("Tornado: Start")
+            # this is a child
+            logger.debug("Tornado: Starting in child")
             super(HTTPServer, self).start()
             self._setup_webapp()
-            ioloop = IOLoop.instance()
-            ioloop.start()
+            signal.signal(signal.SIGTERM, self._terminate_handler)
+            self.ioloop = IOLoop.instance()
+            self.ioloop.start()
+        else:
+            # this is parent
+            self._child_pid = pid
+
+    def kill(self):
+        '''
+        Kill the tornado web app. Use when stop is not working.
+        '''
+        logger.debug("Tornado: Kill")
+        super(HTTPServer, self).stop()
+        os.kill(self._child_pid, signal.SIGKILL)
+        logger.debug("Tornado: Killed")
 
     def stop(self):
         ''' Stop a run tornado web app '''
-        # FIXME: THIS ISN"T WORKING!
-        # should catch sigkill/sigterm and shutdown properly
-        super(HTTPServer, self).stop()
         logger.debug("Tornado: Stop")
-        ioloop = IOLoop.instance()
-        ioloop.stop()
-        if hasattr(self, '_web_app'):
-            del self._web_app
+        super(HTTPServer, self).stop()
+        os.kill(self._child_pid, signal.SIGTERM)
+
+    def _terminate_handler(self, sig, frame):
+        logger.debug("Tornado: Recieved stop signal")
+        logger.debug("Tornado: Wait...")
+        self.ioloop.add_callback(self._stop_server)
+
+    def _stop_server(self):
+        self.server.stop()
+        self.ioloop.add_timeout(time.time() + 5.0,
+                                self._stop_ioloop)
+
+    def _stop_ioloop(self):
+        self.ioloop.stop()
+        logger.debug("Tornado: Stopped")
+        # suicide:
+        os.kill(os.getpid(), signal.SIGKILL)
