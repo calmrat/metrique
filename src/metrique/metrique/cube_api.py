5690750990492cd3aec69b002a80908a067661d2
Juraj Niznan <jurajniznan@gmail.com>
2013-10-14 16:42:01 +0200

    activity history import refactor

--- src/metrique/metrique/cube_api.py
+++ src/metrique/metrique/cube_api.py
@@ -202,6 +202,123 @@ def remove(self, ids, cube=None, backup=False, owner=None):
     return sorted(result)
 
 
+######## ACTIVITY IMPORT ########
+
+def activity_import(self, oids=None, chunk_size=1000, cube=None, owner=None):
+    '''
+    WARNING: Do NOT run extract while activity import is running,
+             it might result in data corruption.
+    Run the activity import for a given cube, if the cube supports it.
+
+    Essentially, recreate object histories from
+    a cubes 'activity history' table row data,
+    and dump those pre-calcultated historical
+    state object copies into the timeline.
+
+    :param object ids:
+        - None: import for all ids
+        - list of ids: import for ids in the list
+    :param int chunk_size:
+        Size of the chunks into which the ids are split, activity import is
+        done and saved separately for each batch
+    '''
+    if oids is None:
+        oids = self.find('_oid == exists(True)', fields='_oid', date='~',
+                         cube=cube, owner=owner)
+        oids = list(oids._oid.unique())
+
+    for i in range(0, len(oids), chunk_size):
+        _activity_import(self, oids[i:i + chunk_size], cube=cube, owner=owner)
+
+
+def _activity_import(self, oids, cube, owner):
+    # get time docs cursor
+    time_docs = self.find('_oid in %s' % oids, fields='__all__', date='~',
+                          sort=[('_oid', 1), ('_start', 1)], raw=True,
+                          merge_versions=False, cube=cube, owner=owner)
+
+    # dict, has format: oid: [(when, field, removed, added)]
+    activities = self.activity_get(oids)
+
+    remove_ids = []
+    save_objects = []
+    for time_doc in time_docs:
+        _oid = time_doc['_oid']
+        _id = time_doc.pop('_id')
+        time_doc.pop('_hash')
+        # we want to update only the oldest version of the object
+        acts = activities.setdefault(_oid, [])
+        updates = _activity_import_doc(self, time_doc, acts)
+        if updates:
+            save_objects += updates
+            remove_ids.append(_id)
+
+    cube.cube_remove(ids=remove_ids)
+    cube.cube_save(save_objects)
+
+
+def _activity_import_doc(self, time_doc, activities):
+    '''
+    Import activities for a single document into timeline.
+    '''
+    batch_updates = [time_doc]
+    # We want to consider only activities that happend before time_doc
+    # do not move this, because time_doc._start changes
+    # time_doc['_start'] is a timestamp, whereas act[0] is a datetime
+    td_start = ts2dt(time_doc['_start'])
+    activities = filter(lambda act: (act[0] < td_start and
+                                     act[1] in time_doc), activities)
+    # make sure that activities are sorted by when descending
+    activities = sorted(activities, reverse=True)
+    for when, field, removed, added in activities:
+        removed = dt2ts(removed) if isinstance(removed, datetime) else removed
+        added = dt2ts(added) if isinstance(added, datetime) else added
+        last_doc = batch_updates.pop()
+        # check if this activity happened at the same time as the last one,
+        # if it did then we need to group them together
+        if last_doc['_end'] == when:
+            new_doc = last_doc
+            last_doc = batch_updates.pop()
+        else:
+            new_doc = deepcopy(last_doc)
+            new_doc.pop('_id') if '_id' in new_doc else None
+            new_doc['_start'] = when
+            new_doc['_end'] = when
+            last_doc['_start'] = when
+        last_val = last_doc[field]
+        new_val, inconsistent = _activity_backwards(new_doc[field],
+                                                    removed, added)
+        new_doc[field] = new_val
+        # Check if the object has the correct field value.
+        if inconsistent:
+            msg = 'Inconsistency: %s %s: %s -> %s, object has %s' % (
+                last_doc['_oid'], field, removed, added, last_val)
+            self.logger.debug(msg)
+            msg = '        Types: %s -> %s, object has %s.' % (
+                type(removed), type(added), type(last_val))
+            self.logger.debug(msg)
+            if '_corrupted' not in new_doc:
+                new_doc['_corrupted'] = {}
+            new_doc['_corrupted'][field] = added
+        # Add the objects to the batch
+        batch_updates.append(last_doc)
+        batch_updates.append(new_doc)
+    # try to set the _start of the first version to the creation time
+    try:
+        # set start to creation time if available
+        last_doc = batch_updates[-1]
+        creation_field = self.get_property('cfield')
+        creation_ts = last_doc[creation_field]
+        if creation_ts < last_doc['_start']:
+            last_doc['_start'] = creation_ts
+        elif len(batch_updates) == 1:
+            # we have only one version, that we did not change
+            return []
+    except:
+        pass
+    return batch_updates
+
+
 def _activity_backwards(val, removed, added):
     if isinstance(added, list) and isinstance(removed, list):
         val = [] if val is None else val
@@ -216,132 +333,3 @@ def _activity_backwards(val, removed, added):
         inconsistent = val != added
         val = removed
     return val, inconsistent
-
-
-def _activity_import_doc(cube, time_doc, activities):
-    '''
-    Import activities for a single document into timeline.
-    '''
-    batch_updates = [time_doc]
-    # We want to consider only activities that happend before time_doc
-    # do not move this, because time_doc._start changes
-    # time_doc['_start'] is a timestamp, whereas act[0] is a datetime
-    td_start = ts2dt(time_doc['_start'])
-    activities = filter(lambda act: (act[0] < td_start and
-                                     act[1] in time_doc), activities)
-    for when, field, removed, added in activities:
-        removed = dt2ts(removed) if isinstance(removed, datetime) else removed
-        added = dt2ts(added) if isinstance(added, datetime) else added
-        last_doc = batch_updates.pop()
-        # check if this activity happened at the same time as the last one,
-        # if it did then we need to group them together
-        if last_doc['_end'] == when:
-            new_doc = last_doc
-            last_doc = batch_updates.pop()
-        else:
-            try:
-                # set start to creation time if available
-                creation_field = cube.get_property('cfield')
-                start = last_doc[creation_field]
-            except:
-                start = when
-            new_doc = deepcopy(last_doc)
-            new_doc.pop('_id') if '_id' in new_doc else None
-            new_doc['_start'] = start
-            new_doc['_end'] = when
-            last_doc['_start'] = when
-        last_val = last_doc[field]
-        new_val, inconsistent = _activity_backwards(new_doc[field],
-                                                    removed, added)
-        new_doc[field] = new_val
-        # Check if the object has the correct field value.
-        if inconsistent:
-            msg = 'Inconsistency: %s %s: %s -> %s, object has %s' % (
-                last_doc['_oid'], field, removed, added, last_val)
-            cube.logger.debug(msg)
-            msg = '        Types: %s -> %s, object has %s.' % (
-                type(removed), type(added), type(last_val))
-            cube.logger.debug(msg)
-            if '_corrupted' not in new_doc:
-                new_doc['_corrupted'] = {}
-            new_doc['_corrupted'][field] = added
-        # Add the objects to the batch
-        batch_updates.append(last_doc)
-        batch_updates.append(new_doc)
-    return batch_updates
-
-
-def _activity_import(cube, oids):
-    # get time docs cursor
-    if isinstance(oids, list):
-        q = '_oid in %s' % oids
-    if isinstance(oids, tuple):
-        q = '_oid >= %s and _oid <= %s' % oids
-    time_docs = cube.find(q, fields='__all__', date='~',
-                          sort=[('_oid', 1), ('_start', 1)], raw=True)
-
-    # generator that yields by ids ascending
-    # has format: (id, [(when, field, removed, added)])
-    act_generator = cube.activity_get(oids)
-
-    last_doc_id = -1
-    aid = -1
-    remove_ids = []
-    save_objects = []
-    for time_doc in time_docs:
-        _oid = time_doc['_oid']
-        _id = time_doc.pop('_id')
-        time_doc.pop('_hash')
-        # we want to update only the oldest version of the object
-        while aid < _oid:
-            aid, acts = act_generator.next()
-        if _oid != last_doc_id:
-            last_doc_id = _oid
-            if aid == _oid:
-                updates = _activity_import_doc(cube, time_doc, acts)
-                if len(updates) > 0:
-                    save_objects += updates
-                    remove_ids.append(_id)
-
-    cube.cube_remove(ids=remove_ids)
-    cube.cube_save(save_objects)
-
-
-def activity_import(self, ids=None, chunk_size=1000):
-    '''
-    WARNING: Do NOT run extract while activity import is running,
-             it might result in data corruption.
-    Run the activity import for a given cube, if the cube supports it.
-
-    Essentially, recreate object histories from
-    a cubes 'activity history' table row data,
-    and dump those pre-calcultated historical
-    state object copies into the timeline.
-
-    :param object ids:
-        - None: import for all ids
-        - list of ids: import for ids in the list
-        - csv list of ids:  import for ids in the csv list
-        - 2-tuple of ids: import for the ids in the interval
-          specified by the tuple
-    :param int save_batch_size:
-        Determines the size of the batch when sending objects to save to the
-        Metrique server
-    :param int chunk_size:
-        Size of the chunks into which the ids are split, activity import is
-        done and saved separately for each batch
-    '''
-    if ids is None:
-        max_oid = self.find('_oid == exists(True)', date='~',
-                            sort=[('_oid', -1)], one=True, raw=True)['_oid']
-        ids = (0, max_oid)
-    if isinstance(ids, tuple):
-        for i in range(ids[0], ids[1] + 1, chunk_size):
-            _activity_import(self, (i, min(ids[1], i + chunk_size - 1)))
-    else:
-        if not isinstance(ids, list):
-            raise ValueError(
-                "Expected ids to be None, tuple or list. Got %s" % type(list))
-
-        for i in range(0, len(ids), chunk_size):
-            _activity_import(self, ids[i:i + chunk_size])
