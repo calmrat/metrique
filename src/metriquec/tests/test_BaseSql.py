6f9f8e3cba267bd0b2ba415ccbd38c40867f769d
Chris Ward <cward@redhat.com>
2013-10-17 16:38:41 +0200

    fixed metriquec.sql.basesql tests

--- src/metriquec/tests/test_BaseSql.py
+++ src/metriquec/tests/test_BaseSql.py
@@ -5,7 +5,9 @@ from metriquec.sql.basesql import BaseSql
 
 callable_test = lambda row_limit: BaseSql(row_limit)
 
-basesql = BaseSql()
+
+def init_basesql():
+    return BaseSql()
 
 
 def try_except(func, exceptions, **kwargs):
@@ -17,24 +19,13 @@ def try_except(func, exceptions, **kwargs):
 
 def test_proxy():
     ' By default, proxy is not implemented '
-    try_except(lambda: basesql.proxy, NotImplementedError)
-
-
-def test_cursor():
-    '''
-        By default, cursor (get from proxy) is unreachable (proxy not
-        implemented
-    '''
-    try_except(lambda: basesql.cursor, NotImplementedError)
-
-
-def test_fetchall():
-    ' By default, trying to get proxy will raise not implemented '
-    try_except(lambda: basesql.fetchall("", 5), NotImplementedError)
+    basesql = init_basesql()
+    try_except(lambda: basesql.get_proxy(), NotImplementedError)
 
 
 def test_validate_row_limit():
     ' check that row is an int or exception'
+    basesql = init_basesql()
     try_except(lambda: basesql._validate_row_limit('a'), TypeError)
 
     assert isinstance(basesql._validate_row_limit(50.0), int)
