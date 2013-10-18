3665ae8c0c5e4c50e27b2df44ffa894306dd7c8e
Chris Ward <cward@redhat.com>
2013-10-18 15:10:32 +0200

    removed row_limit tests (obsolete)

--- src/metriquec/tests/test_BaseSql.py
+++ src/metriquec/tests/test_BaseSql.py
@@ -3,7 +3,7 @@
 
 from metriquec.sql.basesql import BaseSql
 
-callable_test = lambda row_limit: BaseSql(row_limit)
+callable_test = lambda: BaseSql()
 
 
 def init_basesql():
@@ -21,11 +21,3 @@ def test_proxy():
     ' By default, proxy is not implemented '
     basesql = init_basesql()
     try_except(lambda: basesql.get_proxy(), NotImplementedError)
-
-
-def test_validate_row_limit():
-    ' check that row is an int or exception'
-    basesql = init_basesql()
-    try_except(lambda: basesql._validate_row_limit('a'), TypeError)
-
-    assert isinstance(basesql._validate_row_limit(50.0), int)
