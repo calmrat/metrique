a73a00ee0f396eb4075ee244dde15ab86d2ec636
Juraj Niznan <jurajniznan@gmail.com>
2013-12-04 15:33:59 +0100

    plotrique - new report class

--- src/plotrique/plotrique/__init__.py
+++ src/plotrique/plotrique/__init__.py
@@ -1,4 +1,5 @@
 from plotting import Plotter, DiffPlotter
 from container import Container
+from report import Report
 
-__all__ = ['Plotter', 'DiffPlotter', 'Container']
+__all__ = ['Plotter', 'DiffPlotter', 'Container', 'Report']
